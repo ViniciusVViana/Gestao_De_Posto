af23cfff6e3e3ebd52d94b53c48c178769ea464c
Guilherme Augusto Deitos Alves <65348931+GuilhermeDeitos@users.noreply.github.com>
2023-03-26 21:19:19 -0300

    teste

--- funcoes.h
+++ funcoes.h
@@ -5,13 +5,21 @@
 #include<locale.h>
 #include<string.h>
 
-struct endereco{
+struct Endereco{
 
-    char rua;
-    char bairro;
-    char cidade;
-    char cep;
+    char[99] rua;
+    char[99] bairro;
+    char[99] cidade;
+    char[15] cep;
     int num_telefone;
     int id_func;
+
+};
+
+struct Categoria{
+    
+    int id;
+    int idPai;
+    char[99] nome;
 
 };
