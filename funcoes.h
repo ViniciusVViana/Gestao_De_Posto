9d020b3d8b1d7a2cf1f08e31bdcdffeb23f9f56c
Vinicius.Viana <viniciusvianav1099@gmail.com>
2023-03-26 21:32:13 -0300

    Structs montados.

--- funcoes.h
+++ funcoes.h
@@ -5,21 +5,37 @@
 #include<locale.h>
 #include<string.h>
 
+typedef struct Categorias Categorias;
+typedef struct Endereço Endereço;
+typedef struct Produtos Produtos;
+
 struct Endereco{
 
-    char[99] rua;
-    char[99] bairro;
-    char[99] cidade;
-    char[15] cep;
+    char rua[99];
+    char bairro[99];
+    char cidade[99];
+    char cep[15];
     int num_telefone;
     int id_func;
 
 };
 
-struct Categoria{
+struct Categorias{
     
     int id;
     int idPai;
-    char[99] nome;
+    char nome[99];
+
+};
+
+struct Produtos{
+
+    int id;
+    Categorias cat;
+    char nome[99];
+    int quantidade;
+    float preco;
+    int entrada;
+    int validade;
 
 };
