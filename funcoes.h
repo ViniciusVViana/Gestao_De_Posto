04e706aa007911ee00f447db75e31045ddab86e4
Vinicius.Viana <viniciusvianav1099@gmail.com>
2023-03-26 21:11:36 -0300

    adicionando arquivos principais.

---
+++ funcoes.h
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+#include<locale.h>
+#include<string.h>
+
+struct endereco{
+
+    char rua;
+    char bairro;
+    char cidade;
+    char cep;
+    int num_telefone;
+    int id_func;
+
+};
---
+++ main.c
@@ -0,0 +1,5 @@
+void main(){
+
+    system("pause");
+
+};
